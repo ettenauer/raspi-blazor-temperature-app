0cefff32c3d2885ded45d0226d1f1f87189bd982
pettenauer <ettenauer@outlook.com>
2021-07-07 11:26:33 +0200

    switch to old library

--- source/Edge/program.py
+++ source/Edge/program.py
@@ -1,6 +1,11 @@
-import board
-import adafruit_dht
+import Adafruit_DHT
 
-dhtDevice = adafruit_dht.DHT22(board.D7)
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 17
 
-print("Temp: {:.1f} C ".format(dhtDevice.temperature))
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+if humidity is not None and temperature is not None:
+   print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+else:
+   print("Failed to retrieve data from humidity sensor")

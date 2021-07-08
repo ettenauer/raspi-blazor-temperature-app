f215b92a4030289aa13e1d995a30a579d72f3ce7
pettenauer <ettenauer@outlook.com>
2021-07-08 12:51:21 +0200

    update docker compose and program.py

--- source/Edge/program.py
+++ source/Edge/program.py
@@ -1,8 +1,11 @@
 from pigpio_dht import DHT22
+import time
 
 gpio = 17 
 
 sensor = DHT22(gpio)
 
-result = sensor.read()
-print(result)
+while True:
+    result = sensor.sample(samples=5)
+    print(result)
+    time.sleep(10)
